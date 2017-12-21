340ae008232b20668e9568eee34bf141485ec6ea
KevinvHelden <k.v.helden96@hotmail.com>
2017-12-21 15:05:25 +0100

    1ste functie
    1ste functie om functionaliteit te testen van interactie met Leap Motion.

---
+++ Hello.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+	public Text CollisionText;
+
+	void Start () {
+		CollisionText.text = "";
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag("box"))
+		{
+			CollisionText.text = "Hello";
+		}
+	}
+}
